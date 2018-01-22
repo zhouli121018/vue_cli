d053fde4ee3740c91635e4bc02f27e3eaf6fb982
zhouli121018 <17607619682@163.com>
2018-01-22 15:01:27 +0800

    project

---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="login">
+      <div class="login_wrapper">
+      <div class="animate form login_form">
+        <section class="login_content">
+          <form>
+            <h1>管理员登录</h1>
+            <div>
+              <input type="text" class="form-control" v-model="userName" placeholder="管理员登录名" required />
+            </div>
+            <div>
+              <input type="password" class="form-control" v-model="userPwd" placeholder="管理员登录密码" required />
+            </div>
+            <div>
+              <a class="btn btn-default submit" @click="loginToIndex">&nbsp;登&nbsp;录&nbsp;</a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="separator">
+              <div class="clearfix"></div>
+              <br />
+
+              <div>
+                <h1><i><img src="../assets/img/favicon.ico"></i> 学子商城后台管理系统</h1>
+                <p>©2017 版权所有，CODEBOY.COM</p>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data:function(){
+      return {
+          msg:'Hello Login',
+          userName:'',
+          userPwd:''
+      }
+  },
+  mounted:function(){
+      console.log('挂载完毕');
+      
+  },
+  methods:{
+      loginToIndex:function(){
+            console.log(this.userName,this.userPwd);
+            this.$http.get('http://Jsonplaceholder.typicode.com/posts?userName='+this.userName).then(function(response){
+                 console.log(response.data);
+                 this.$router.push('index');
+            });
+      }
+  }
+}
+</script>
+<style>
+
+</style>
+
+
---
+++ src/components/UserList.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+      <div class="right_col" role="main">
+      <div class="row">
+        <div class="col-xs-12 x_title">
+          <h3>用户列表</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-3">
+          <form class="form-inline">
+            <div class="form-group">
+              <label for="page-size">每页显示记录数：</label>
+              <select class="form-control" id="page-size">
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="40">40</option>
+                <option value="60">60</option>
+                <option value="80">80</option>
+                <option value="100">100</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div class="col-xs-4 col-xs-offset-5">
+          <div class="form-group has-feedback">
+            <label for="product-kw" class="sr-only">搜索关键字：</label>
+            <input class="form-control" type="text" id="product-kw" placeholder="请输入搜索关键字">
+            <span class="glyphicon glyphicon-search form-control-feedback"></span>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="table-responsive">
+            <table class="table table-hover table-striped" id="table-laptop">
+              <thead>
+              <tr>
+                <th>
+                  <div class="checkbox" style="margin: 0;">
+                    <label>
+                      <input type="checkbox">全选
+                    </label>
+                  </div>
+                </th>
+                <th>编号</th>
+                <th>头像</th>
+                <th>登录名</th>
+                <th>用户名</th>
+                <th>性别</th>
+                <th>邮箱</th>
+                <th>电话</th>
+                <th>操作</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td colspan="8">
+                  <div class="loading">
+                    <img src="../assets/img/loading.gif" alt="">
+                  </div>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="row">
+            <div class="col-xs-12">
+              <ul class="pagination pull-right" id="pagination">
+                <li><a href="#">上一页</a></li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li class="active"><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">下一页</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data:function(){
+      return {
+          msg:'Hello UserList'
+      }
+  }
+}
+</script>
+<style>
+
+</style>
+
+
